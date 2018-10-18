7f0c0033995861f728ecb4b6b3abdd4471b8687d
Thomas MacLean <thomas@thomasmaclean.com>
2018-10-18 15:56:35 -0400

    add vendor file handling

--- bin/apex-web-upgrade.js
+++ bin/apex-web-upgrade.js
@@ -2,4 +2,4 @@
 const shell = require('shelljs');
 
 shell.cp('-R', 'node_modules/apex-web-tools/static', '.');
-shell.cp('node_modules/apex-web-tools/polyfills.js', '.')
+shell.cp('node_modules/apex-web-tools/polyfills.js', './vendor/')

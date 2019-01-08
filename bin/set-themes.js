ae51371388170b60d9f439b35ad997e2ba0be1fa
Martin Ortiz <martuanez@gmail.com>
2019-01-08 11:27:48 -0300

    Added script for gathering all theme files and placing those on APEXWebConfig.global.themes prop on local/config.js file

---
+++ bin/set-themes.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+
+global.window = {};
+require(path.join('..', 'local', 'config.js'));
+
+let files = fs.readdirSync(path.join('local', 'themes'));
+files = files.filter(f => f.split('.').pop() === 'css')
+  .map(fileName => fileName.replace('.css', ''));
+
+window.APEXWebConfig.global.themes = files;
+console.log(files);
+
+
+const configContent = JSON.stringify(window.APEXWebConfig, null, 2);
+
+
+console.log(configContent);
+
+fs.writeFileSync(
+  path.join('local', 'config.js'),
+  `window.APEXConfig = ${configContent};`,
+  'utf-8'
+);

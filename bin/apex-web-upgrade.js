569bdd44733b907a46eec89e0e89444d6f8e562e
Martin Ortiz <martuanez@gmail.com>
2019-01-09 14:07:04 -0300

    AW 2.5.9 static code

--- bin/apex-web-upgrade.js
+++ bin/apex-web-upgrade.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
+const path = require('path');
 const shell = require('shelljs');
+const awtStaticFolder = path.join('node_modules', 'apex-web-tools', 'static');
+const awtPolyfills = path.join('node_modules', 'apex-web-tools', 'polyfills.js');
+const vendorFolder = path.join('.', 'vendor');
+const staticFolder = path.join('.', 'static');
 
-shell.cp('-R', 'node_modules/apex-web-tools/static', '.');
-shell.cp('node_modules/apex-web-tools/polyfills.js', './vendor/')
+shell.rm('-rf', staticFolder);
+shell.cp('-R', awtStaticFolder, '.');
+shell.cp(awtPolyfills, vendorFolder);
